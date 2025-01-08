fbfbf1bcde3df8f69d78f4dbe818e9f50016bac5
MartinBINARD <martin.binard.dev@gmail.com>
2025-01-07 18:12:51 -1000

    Add favorites API RTK query reducers

---
+++ store/api/favoritesApi.js
@@ -0,0 +1,42 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const favoritesApi = createApi({
+  reducerPath: "favoritesApi",
+  baseQuery: fetchBaseQuery({ baseUrl: EXPO_PUBLIC_API_URL }),
+  tagTypes: ["Favorites"],
+  endpoints: (build) => ({
+    getAllFavorites: build.query({
+      providesTags: ["Favorites"],
+      query: () => "favorites.json",
+      transformResponse: (response) => {
+        const favorites = [];
+        for (const key in response) {
+          const favorite = {
+            id: key,
+            ...response[key]
+          };
+          favorites.push(favorite);
+        }
+
+        return favorites;
+      }
+    }),
+    addFavorite: build.mutation({
+      invalidatesTags: "Favorites",
+      query: (shoesId) => ({
+        url: "favorites.json",
+        method: "POST",
+        body: shoesId
+      })
+    }),
+    removeFavorite: build.mutation({
+      invalidatesTags: "Favorites",
+      query: ({ id }) => ({
+        url: `favorites/${id}.json`,
+        method: "DELETE"
+      })
+    })
+  })
+});
+
+export const { useGetAllFavoritesQuery, useAddFavoriteMutation, useRemoveFavoriteMutation} = favoritesApi;
