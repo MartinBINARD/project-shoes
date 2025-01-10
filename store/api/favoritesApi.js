073e06cedbd3483e72395a2cfdff10aecee97bb6
MartinBINARD <martin.binard.dev@gmail.com>
2025-01-09 17:47:13 -1000

    Add pessimistic and optimistic update to addFavorite and updateFavorites queries

--- store/api/favoritesApi.js
+++ store/api/favoritesApi.js
@@ -3,10 +3,10 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const favoritesApi = createApi({
     reducerPath: 'favoritesApi',
     baseQuery: fetchBaseQuery({ baseUrl: process.env.EXPO_PUBLIC_API_URL }),
-    tagTypes: ['Favorites'],
+    // tagTypes: ['Favorites'],
     endpoints: (build) => ({
         getAllFavorites: build.query({
-            providesTags: ['Favorites'],
+            // providesTags: ['Favorites'],
             query: () => 'favorites.json',
             transformResponse: (response) => {
                 const favorites = {};
@@ -18,20 +18,46 @@ export const favoritesApi = createApi({
             },
         }),
         addFavorite: build.mutation({
-            invalidatesTags: ['Favorites'],
+            // invalidatesTags: ['Favorites'],
             query: (shoesId) => ({
                 url: 'favorites.json',
                 method: 'POST',
                 body: [shoesId],
             }),
+            async onQueryStarted(shoesId, { dispatch, queryFulfilled }) {
+                try {
+                    const { data } = await queryFulfilled;
+                    const patchResult = dispatch(
+                        favoritesApi.util.upsertQueryData('getAllFavorites', undefined, {
+                            id: data.name,
+                            shoesIds: [shoesId],
+                        }),
+                    );
+                } catch {}
+            },
         }),
         updateFavorites: build.mutation({
-            invalidatesTags: ['Favorites'],
+            // invalidatesTags: ['Favorites'],
             query: ({ id, shoesIds }) => ({
                 url: `favorites/${id}.json`,
                 method: 'PUT',
                 body: shoesIds,
             }),
+            async onQueryStarted(arg, { dispatch, queryFulfilled }) {
+                const patchResult = dispatch(
+                    favoritesApi.util.updateQueryData('getAllFavorites', undefined, (draft) => {
+                        if (arg.shoesIds?.length === 0) {
+                            draft.id = null;
+                        }
+                        draft.shoesIds = arg.shoesIds;
+                    }),
+                );
+                try {
+                    await queryFulfilled;
+                } catch {
+                    patchResult.undo();
+                }
+            },
         }),
     }),
 });
